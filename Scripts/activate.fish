5c5bfd0cedfab525a34ff8b6c3ef6804a885e786
Aime <aimeca011.com>
2025-03-03 18:14:57 -0600

    Primer commit - 19590038

--- Scripts/activate.fish
+++ Scripts/activate.fish
@@ -33,11 +33,11 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV 'C:\Users\Aime Sykes\Documents\EX_U1_19590338\examen'
+set -gx VIRTUAL_ENV 'C:\Users\Aime Sykes\Documents\EX_U1_19590338'
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/"Scripts $PATH
-set -gx VIRTUAL_ENV_PROMPT examen
+set -gx VIRTUAL_ENV_PROMPT EX_U1_19590338
 
 # Unset PYTHONHOME if set.
 if set -q PYTHONHOME
@@ -57,7 +57,7 @@ if test -z "$VIRTUAL_ENV_DISABLE_PROMPT"
         set -l old_status $status
 
         # Output the venv prompt; color taken from the blue of the Python logo.
-        printf "%s(%s)%s " (set_color 4B8BBE) examen (set_color normal)
+        printf "%s(%s)%s " (set_color 4B8BBE) EX_U1_19590338 (set_color normal)
 
         # Restore the return status of the previous command.
         echo "exit $old_status" | .
